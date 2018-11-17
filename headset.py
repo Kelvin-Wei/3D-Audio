5cd237ad251ab372900e8a29937fee0adecd5127
Ritchie <ac.ritchiew@gmail.com>
2018-11-16 21:38:55 -0500

    3D Audio

--- headset.py
+++ headset.py
@@ -1,105 +1,101 @@
 import os, sys
-import wave, struct, matplotlib
+import wave, struct
+import argparse
 import numpy as np
 from scipy import signal
-import matplotlib.pyplot as plt
 
-def read_wav(name):
-    wav = wave.open(name, 'r')
-    params = wav.getparams()
-    nframes = params[3]
-    wav_str = wav.readframes(nframes)
-    wav.close()
-    return wav_str, params
+parser = argparse.ArgumentParser()
+parser.add_argument("--input","-i")
+parser.add_argument("--output","-o",type=str,default="3d_output.wav")
+parser.add_argument("--rate","-r",type=int,default=12)
+parser.add_argument("--level","-l",type=int,choices=range(-40, 100, 10),default=0)
+parser.add_argument("--clockwise","-c",action="store_true")
+parser.set_defaults(clockwise=False)
 
-def write_wav(name, string, params):
-    wav = wave.open(name, 'w')
-    wav.setparams(params)
-    wav.writeframes(string)
-    wav.close()
+class Audio3D():
 
-def string_to_data(string):
-    return np.fromstring(string, dtype=np.short).reshape((-1,2)).T.astype(np.float32)
+    def __init__(self):
+        self.__load_hrtf()
 
-def data_to_string(data):
-    return data.T.reshape((-1,)).astype(np.short).tostring()
+    def __str2dat(self, string):
+        return np.fromstring(string, dtype=np.short).reshape((-1,2)).T.astype(np.float32)
 
-def read_hrtf(angle):
-    path = 'res/HRTF-full/elev0'
-    left_template = 'L0eXXXa.dat'
-    right_template = 'R0eXXXa.dat'
-    angle_str = str(angle)
-    while len(angle_str) < 3:
-        angle_str = '0'+angle_str
-    left_file_name = left_template.replace('XXX', angle_str)
-    right_file_name = right_template.replace('XXX', angle_str)
-    hrtf_file = open(os.path.join(path, left_file_name), 'rb')
-    left_buf = hrtf_file.read(1024)
-    hrtf_file.close()
-    hrtf_file = open(os.path.join(path, right_file_name), 'rb')
-    right_buf = hrtf_file.read(1024)
-    hrtf_file.close()
-    left_data = []
-    right_data = []
-    for i in range(0,1024,2):
-        left_data.append(struct.unpack('>h', left_buf[i:i+2])[0])
-        right_data.append(struct.unpack('>h', right_buf[i:i+2])[0])
-    return np.array([left_data, right_data]).reshape((-1,))
+    def __dat2str(self, data):
+        return data.T.reshape((-1,)).astype(np.short).tostring()
 
-def preload_hrtf():
-    hrtf = read_hrtf(0)
-    for i in range(5,360,5):
-        hrtf = np.vstack((hrtf, read_hrtf(i)))
-    return hrtf
+    def __angle2id(self, angle):
+        return angle / 5
 
-def angle_to_index(angle):
-    return angle/5
+    def __load_hrtf(self):
+        self.__hrtf_dict = {}
+        level_list = range(-40, 100, 10)
+        for lv in level_list:
+            level_name = "level%d" % lv
+            self.__hrtf_dict[level_name] = np.load("res/level%d.npy" % lv)
 
-def filter(hrtf, wav, start, end):
-    blank = end - start
-    s = max(0,start - blank)
-    hrtf = hrtf.reshape((2,-1))
-    wav_data = wav[:,s:end]
-    new_wav = np.zeros((2,end-s))
-    new_wav[0,:] = signal.lfilter(hrtf[0], np.array([1.]), wav_data[0,:])
-    new_wav[1,:] = signal.lfilter(hrtf[1], np.array([1.]), wav_data[1,:])
-    if new_wav.shape[1] == end - start:
-        return new_wav
-    return new_wav[:,blank:]
+    def __load_audio(self, audio_file):
+        if not audio_file.endswith(".wav"):
+            assert("No *.wav file is found.")
+        wav_file = wave.open(audio_file, 'r')
+        wav_params = wav_file.getparams()
+        wav_nframes = wav_params[3]
+        wav_raw = wav_file.readframes(wav_nframes)
+        wav_file.close()
+        return self.__str2dat(wav_raw), wav_params
 
-# body
-file_name = sys.argv[1]
-new_name = file_name.replace('.wav', '_sample.wav')
-wav_str, wav_params = read_wav(file_name)
-wav_data = string_to_data(wav_str)
-wav_max = np.max(wav_data)
+    def __save_audio(self, outp_file, params, data):
+        wav_file = wave.open(outp_file, 'w')
+        params = list(params)
+        wav_file.setparams(params)
+        wav_file.writeframes(self.__dat2str(data))
+        wav_file.close()
 
-angle = 135
-flip_angle = 360-angle
-if flip_angle == 360:
-    flip_angle = 0
-hrtf_data1 = read_hrtf(angle)
-hrtf_data2 = read_hrtf(flip_angle)
+    def __norm(self, data):
+        data_scale = np.max(np.abs(data))
+        return data / data_scale, data_scale
 
-new_wav = np.zeros((2,wav_params[3]))
-new_wav_params = list(wav_params)
+    def __filter(self, wav_data, hrtf_data, start, end):
+        blank = end - start
+        st = max(0, start - blank)
+        hrtf_data = hrtf_data.reshape((2,-1))
+        wav_slide = wav_data[:,st:end]
+        outp_slide = np.zeros((2,end-st))
+        outp_slide[0,:] = signal.lfilter(hrtf_data[0], np.array([1.]), wav_slide[0,:])
+        outp_slide[1,:] = signal.lfilter(hrtf_data[1], np.array([1.]), wav_slide[1,:])
+        if outp_slide.shape[1] != end - start:
+            outp_slide = outp_slide[:,blank:]
+        return outp_slide
 
-framerate = wav_params[2]
-nframes = wav_params[3]
-times = nframes/framerate
+    def __run(self, wav_data, hrtf_data, duration, nframes, frame_per_rate, rate, direction):
+        outp_track = np.zeros((2, nframes))
+        angle_id = 0
+        boundary = hrtf_data.shape[0]
+        for i in range(duration * rate):
+            start = i * frame_per_rate
+            end = min(start + frame_per_rate, nframes)
+            angle_id += direction
+            angle_id += boundary
+            angle_id %= boundary
+            outp_track[:,start:end] = self.__filter(wav_data, hrtf_data[angle_id], start, end)
+        return outp_track
 
-hrtf_data = preload_hrtf()
+    def perform(self, audio_file, rate, level, direction, outp_file):
+        track, track_param = self.__load_audio(audio_file)
+        _, track_scale = self.__norm(track)
+        duration = track_param[3] / track_param[2]
+        frame_per_rate = track_param[2] / rate
+        outp_track = self.__run(track, self.__hrtf_dict["level%d" % level], duration, track_param[3], frame_per_rate, rate, direction)
+        outp_track = self.__norm(outp_track)[0] * track_scale
+        if isinstance(outp_file, str):
+            self.__save_audio(outp_file, track_param, outp_track)
+        else:
+            self.__save_audio(outp_file(audio_file), track_param, outp_track)
 
-rate = 12
-for i in range(times*rate):
-    f_rate = framerate//rate
-    start = i*f_rate
-    end = min(start+f_rate, nframes)
-    angle = (i*10)%360
-    new_wav[:,start:end] = filter(hrtf_data[angle_to_index(angle)], wav_data, start, end)
-    # if i > 0:
-    #     new_wav[:,start-1] = new_wav[:,start] = np.mean(new_wav[:,start-1:start+1],axis=0)
-
-new_wav = new_wav/np.max(new_wav)*wav_max*2.
-
-write_wav(new_name, data_to_string(new_wav), new_wav_params)
+if __name__ == "__main__":
+    args = parser.parse_args()
+    audio3d = Audio3D()
+    audio3d.perform(args.input,
+                    args.rate,
+                    args.level,
+                    1 if args.clockwise else -1,
+                    args.output)
